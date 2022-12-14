8ecfc0f7882b4e8b9d46b0fd506c485eba405512
allawean <barbora.plankova@gmail.com>
2022-12-14 18:51:22 +0100

    second part of the puzzle

--- 1/calorie_counting.py
+++ 1/calorie_counting.py
@@ -12,21 +12,31 @@ def open_file(input_file: Path) -> list[str]:
 def split_snacks_per_elf(total_snacks_raw: list[str]) -> np.array:
     return [[eval(x) for x in elf.splitlines()] for elf in total_snacks_raw]
 
-def get_solution(snacks_per_elf: list[list]) -> int:
-    return max([sum(x) for x in snacks_per_elf])
+def get_sums_of_calories_per_elf(snacks_per_elf: list[list[int]]) -> list[int]:
+    return [sum(x) for x in snacks_per_elf]
+
+def sort_elves(total_snacks: list[int]) -> None:
+    total_snacks.sort()
 
 
 def get_snacks(input_file: Path):
     total_snacks_raw = open_file(input_file)
     return split_snacks_per_elf(total_snacks_raw)
 
+def get_max_n_elves(sorted_total_snacks: list[int], number_of_elves: int) -> int:
+    return sum(sorted_total_snacks[-number_of_elves:])
+
 
 
 @click.command()
+@click.option('--number-of-elves', default=3)
 @click.argument('input_file', type=click.Path(exists=True))
-def main(input_file: Path):
+def main(number_of_elves: int, input_file: Path):
     snacks_per_elf = get_snacks(input_file)
-    print(f'Max total calories per elf are: {get_solution(snacks_per_elf)}')
+    calories_per_elf = get_sums_of_calories_per_elf(snacks_per_elf)
+    print(f'Max total calories per elf are: {max(calories_per_elf)}')
+    sort_elves(calories_per_elf)
+    print(f'Max total calories of {number_of_elves} maximal elves are: {get_max_n_elves(calories_per_elf, number_of_elves)}')
 
 
 
